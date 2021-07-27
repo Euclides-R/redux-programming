66429a1f12001c6eff307edf1cd69136d564e536
Euclides-R <euclides.rodriguesjr@outlook.com>
2021-07-27 20:09:36 -0300

    Lógica da calculadora finalizada ➕🤟🤟

--- calculadora/src/componentes/Screen.js
+++
@@ -1,9 +0,0 @@
-import React, { Children } from 'react';
-
-export default function Screen({ children }) {
-    return (
-        <div className="screen-design">
-            {children}
-        </div>
-    );
-}
--- calculadora/src/componentes/index.js
+++ calculadora/src/componentes/index.js
@@ -1,2 +1,2 @@
 export { default as Button } from './Button';
-export { default as Screen } from './Screen';
+export { default as Display } from './Display';
