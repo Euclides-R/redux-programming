53401c2a53bd66f142e63f95bf315cf2ec4e611a
Euclides Rodrigues Jr <euclides.rodriguesjr@outlook.com>
2021-10-12 15:32:18 -0300

    Testando cadastrado com API

--- hooks/src/components/layout/Content.jsx
+++ hooks/src/components/layout/Content.jsx
@@ -1,17 +1,17 @@
-import './Content.css'
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import './Content.css';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import Home from '../../views/examples/Home'
-import NotFound from '../../views/examples/NotFound'
-import UseState from '../../views/examples/UseState'
-import UseEffect from '../../views/examples/UseEffect'
-import UseRef from '../../views/examples/UseRef'
-import UseCallback from '../../views/examples/UseCallback'
-import UseMemo from '../../views/examples/UseMemo'
-import UseContext from '../../views/examples/UseContext'
-import UseReducer from '../../views/examples/UseReducer'
-import UseCustom from '../../views/examples/UseCustom'
+import Home from '../../views/examples/Home';
+import NotFound from '../../views/examples/NotFound';
+import UseState from '../../views/examples/UseState';
+import UseEffect from '../../views/examples/UseEffect';
+import UseRef from '../../views/examples/UseRef';
+import UseCallback from '../../views/examples/UseCallback';
+import UseMemo from '../../views/examples/UseMemo';
+import UseContext from '../../views/examples/UseContext';
+import UseReducer from '../../views/examples/UseReducer';
+import UseCustom from '../../views/examples/UseCustom';
 
 const Content = props => (
     <main className="Content">
@@ -50,4 +50,4 @@ const Content = props => (
     </main>
 )
 
-export default Content
+export default Content;
--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -1,21 +1,11 @@
-import React, { useContext } from 'react'
-import PageTitle from '../../components/layout/PageTitle'
-import SectionTitle from '../../components/layout/SectionTitle'
+import React, { useContext } from 'react';
+import PageTitle from '../../components/layout/PageTitle';
 
-import DataContext from '../../data/DataContext'
-import { AppContext } from '../../data/Store'
+import DataContext from '../../data/DataContext';
 
-const UseContext = (props) => {
-    const { state, setState } = useContext(DataContext)
+const UseContext = props => {
 
-    function addNumber(delta) {
-        setState({
-            ...state,
-            number: state.number + delta,
-        })
-    }
-
-    const { number, text, setNumber } = useContext(AppContext)
+    const context = useContext(DataContext);
 
     return (
         <div className="UseContext">
@@ -23,38 +13,9 @@ const UseContext = (props) => {
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
-
-            <SectionTitle title="Execício #01" />
             <div className="center">
-                <span className="text">{state.text}</span>
-                <span className="text">{state.number}</span>
-
-                <div>
-                    <button className="btn" onClick={() => addNumber(-1)}>
-                        -1
-                    </button>
-                    <button className="btn" onClick={() => addNumber(1)}>
-                        +1
-                    </button>
-                </div>
-            </div>
-
-            <SectionTitle title="Execício #02" />
-            <div className="center">
-                <span className="text">{text}</span>
-                <span className="text">{number}</span>
-                <div>
-                    <button
-                        className="btn"
-                        onClick={() => setNumber(number - 1)}>
-                        -1
-                    </button>
-                    <button
-                        className="btn"
-                        onClick={() => setNumber(number + 1)}>
-                        +1
-                    </button>
-                </div>
+                <span className="text">{context.text}</span>
+                <span className="text">{context.number}</span>
             </div>
         </div>
     )
